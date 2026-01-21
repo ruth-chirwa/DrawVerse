ee513a724047ef7a81aff2be5ba1fe2879ae3c8a
Ruth <chirwaruth647@gmail.com>
2026-01-21 16:48:31 +0200

    feat: update navigation and restore README with project details

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,10 +1,17 @@
 import "./styles/App.css";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <div>
+      <nav style={{ margin: 10 , color: 'blue' }}>
+        <li><Link to="/">Home</Link> | <Link to="/about">About</Link></li>
+      </nav>
+      <hr />
+      {/* Child routes will render here */}
       <Outlet />
+    </div>
     </>
   );
 }
