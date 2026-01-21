7346d3cb6da82722bf6bd1bfdb5fcefe1c60808e
Ruth <chirwaruth647@gmail.com>
2026-01-21 14:28:03 +0200

    feat: add router for navigation

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,29 +1,12 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import './styles/App.css';
-
-import HomePage from './components/Home';
-import AboutPage from './components/About';
-
+import "./styles/App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Router> {/*creates url paths from components*/}
-        <div>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/gallery" element={<div>Gallery</div>} />
-          </Routes>
-        </div>
-      </Router>
-
-      <div>
-        <h1>Hello</h1>
-        <Link to="/navigate">Go here</Link> {/*advanced version of a tag*/}
-      </div>
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
